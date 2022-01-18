d56bc2b3bcfdad7d4958c00c106094e3fd1c8130
Daniel M. Danelli <danieldanelli@gmail.com>
2022-01-18 08:58:43 -0300

    implementação com null resource

--- tf_pckr_ansi/terraform/main.tf
+++ tf_pckr_ansi/terraform/main.tf
@@ -11,26 +11,52 @@ terraform {
   }
 }
 
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
+variable index {}
+variable ami {}
 
-  name = "single-instance"
+resource "null_resource" "exec_packer" {
+  provisioner "local-exec" {
+    command = "bash ./script.sh"
+  }
+}
 
-  ami                    = var.ami
-  instance_type          = "t2.micro"
+resource "aws_instance" "modeloteste" {
+  ami = var.ami
+  instance_type = "t2.micro"
   key_name               = "kp_lab"
   monitoring             = true
   vpc_security_group_ids = ["sg-0c5499b0953260fa8"]
   subnet_id              = "subnet-0b0fa07c52ffc4553"
-
   tags = {
     Terraform   = "true"
     Environment = "dev"
   }
+  provisioner "local-exec" {
+    interpreter = ["bash", "-c"]
+    command = "while [[ $(cat ami) != \"${var.index}\" ]]; do sleep 5; done; sleep 3;"
+  }
 }
 
-#via export TF_VAR
- output "ami_id" {
-   value = var.ami
- }
+// module "ec2_instance" {
+//   source  = "terraform-aws-modules/ec2-instance/aws"
+//   version = "~> 3.0"
+
+//   name = "single-instance"
+
+//   ami                    = var.ami
+//   instance_type          = "t2.micro"
+//   key_name               = "kp_lab"
+//   monitoring             = true
+//   vpc_security_group_ids = ["sg-0c5499b0953260fa8"]
+//   subnet_id              = "subnet-0b0fa07c52ffc4553"
+
+//   tags = {
+//     Terraform   = "true"
+//     Environment = "dev"
+//   }
+// }
+
+// #via export TF_VAR
+//  output "ami_id" {
+//    value = var.ami
+//  }
